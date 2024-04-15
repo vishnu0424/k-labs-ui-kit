4dd82636030ac4cd3742c7324cd463e4945e90cb
vishnu0424 <vishnu.k@kairostech.com>
2024-04-15 20:18:13 +0530

    added header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -7,7 +7,13 @@ import {
   Tooltip,
   useTheme,
   Divider,
+  AppBar as MuiAppBar,
+  Toolbar,
+  Paper,
 } from "@mui/material";
+
+import { styled } from "@mui/material/styles";
+
 import {
   Menu as MenuIcon,
   ChevronLeft as ChevronLeftIcon,
@@ -21,6 +27,24 @@ import { SideBarContext } from "../../utils";
 // import { ColorModeContext } from "../../theme/theme";
 // import AccountSettings from "./AccountSettings";
 // import ServiceStatusPopup from "./ServiceStatus/ServiceStatusPopup";
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: 3,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    // marginLeft: drawerWidth,
+    // width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
 const Header = () => {
   const theme = useTheme();
   const { drawerWidth, setDrawerWidth } = useContext(SideBarContext);
@@ -32,62 +56,86 @@ const Header = () => {
   // const colorMode = useContext(ColorModeContext);
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container direction="row" justifyContent="left" alignItems="center">
-        <Grid item sm={4}>
-          <Box className="v-center">
-            <Box
-              sx={{
-                width: openSideMenu ? `calc(200px - ${24}px)` : " fit-content",
-              }}
-            >
-              <IconButton
-                sx={{ mr: 2 }}
-                size="small"
-                onClick={onClickMenuIcon}
-                edge="start"
+    <AppBar
+      enableColorOnDark
+      position="fixed"
+      open={openSideMenu}
+      className="headerCus"
+      sx={{ backgroundColor: "transparent !important" }}
+    >
+      <Toolbar
+        component={Paper}
+        sx={{ border: 0, boxShadow: 0, borderRadius: 0 }}
+      >
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="left"
+            alignItems="center"
+          >
+            <Grid item sm={4}>
+              <Box className="v-center">
+                <Box
+                  sx={{
+                    width: openSideMenu
+                      ? `calc(200px - ${24}px)`
+                      : " fit-content",
+                  }}
+                >
+                  <IconButton
+                    sx={{ mr: 2 }}
+                    size="small"
+                    onClick={onClickMenuIcon}
+                    edge="start"
+                  >
+                    {openSideMenu ? <ChevronLeftIcon /> : <MenuIcon />}
+                  </IconButton>
+                </Box>
+                <Box>
+                  {/* <img src={Logo} height="26px" alt="logo-notavailable" /> */}
+                  {/* <img src={ClientLogo} height="26px" alt="logo-notavailable" /> */}
+                </Box>
+              </Box>
+            </Grid>
+
+            <Grid item sm={4} textAlign="center"></Grid>
+            <Grid item sm={4} className="headerIcons">
+              {/* <ServiceStatusPopup /> */}
+              <Divider
+                orientation="vertical"
+                variant="string"
+                flexItem
+                style={{ backgroundColor: "#ccc", width: 2 }}
+              />
+
+              <Tooltip
+                arrow
+                placement="top"
+                title={`${theme?.palette?.mode} mode`}
               >
-                {openSideMenu ? <ChevronLeftIcon /> : <MenuIcon />}
-              </IconButton>
-            </Box>
-            <Box>
-              {/* <img src={Logo} height="26px" alt="logo-notavailable" /> */}
-              {/* <img src={ClientLogo} height="26px" alt="logo-notavailable" /> */}
-            </Box>
-          </Box>
-        </Grid>
+                <IconButton
+                  size="small"
+                  sx={{
+                    background: "linear-gradient(45deg, #611EB6, #66A2EE)",
+                    color: "#fff",
+                  }}
+                  // onClick={colorMode.toggleColorMode}
+                >
+                  {theme?.palette?.mode === "dark" ? (
+                    <DarkModeOutlinedIcon fontSize="small" />
+                  ) : (
+                    <LightModeOutlinedIcon fontSize="small" />
+                  )}
+                </IconButton>
+              </Tooltip>
 
-        <Grid item sm={4} textAlign="center"></Grid>
-        <Grid item sm={4} className="headerIcons">
-          {/* <ServiceStatusPopup /> */}
-          <Divider
-            orientation="vertical"
-            variant="string"
-            flexItem
-            style={{ backgroundColor: "#ccc", width: 2 }}
-          />
-
-          <Tooltip arrow placement="top" title={`${theme?.palette?.mode} mode`}>
-            <IconButton
-              size="small"
-              sx={{
-                background: "linear-gradient(45deg, #611EB6, #66A2EE)",
-                color: "#fff",
-              }}
-              // onClick={colorMode.toggleColorMode}
-            >
-              {theme?.palette?.mode === "dark" ? (
-                <DarkModeOutlinedIcon fontSize="small" />
-              ) : (
-                <LightModeOutlinedIcon fontSize="small" />
-              )}
-            </IconButton>
-          </Tooltip>
-
-          {/* <AccountSettings /> */}
-        </Grid>
-      </Grid>
-    </Box>
+              {/* <AccountSettings /> */}
+            </Grid>
+          </Grid>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,61 +1,18 @@
-import { styled } from "@mui/material/styles";
-import {
-  AppBar as MuiAppBar,
-  Box,
-  Drawer as MuiDrawer,
-  Toolbar,
-  Paper,
-} from "@mui/material";
 import Header from "./Header";
 import SideMenu from "./SideMenu";
 import React, { useState, useMemo } from "react";
 import { SideBarContext } from "../../utils";
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  zIndex: 3,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    // marginLeft: drawerWidth,
-    // width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
 const Layout = () => {
   const [drawerWidth, setDrawerWidth] = useState(200);
-  const openSideMenu = drawerWidth === 200;
   const contextValue = useMemo(
     () => ({ setDrawerWidth, drawerWidth }),
     [setDrawerWidth, drawerWidth]
   );
   return (
     <SideBarContext.Provider value={contextValue}>
-      <Box>
-        <AppBar
-          enableColorOnDark
-          position="fixed"
-          open={openSideMenu}
-          className="headerCus"
-          sx={{ backgroundColor: "transparent !important" }}
-        >
-          <Toolbar
-            component={Paper}
-            sx={{ border: 0, boxShadow: 0, borderRadius: 0 }}
-          >
-            <Header />
-          </Toolbar>
-        </AppBar>
-
-        <SideMenu />
-      </Box>
+      <Header />
+      <SideMenu />
     </SideBarContext.Provider>
   );
 };
